050e3ef4a075faa81e500cf06a2396cc0398bdbc
boasmeier <62314872+boasmeier@users.noreply.github.com>
2021-11-03 13:38:50 +0100

    feat(firmware): add SerialLogger

---
+++ src/firmware/doorlock_controller/SerialLogger.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <stdarg.h>
+#include "SerialLogger.hpp"
+#include "DoorlockConfig.h"
+
+static void SerialLogger::info(const char *fmt, ...) {
+  char msg[LOG_SIZE_MAX];
+  strcpy(msg, "INFO: ");
+  char buf[LOG_SIZE_MAX - strlen(msg)];
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf_P(buf, LOG_SIZE_MAX, fmt, ap);
+  va_end(ap);
+  strcat(msg, buf);
+  Serial.println(msg);
+}
+
+static void SerialLogger::error(const char *fmt, ...) {
+  char msg[LOG_SIZE_MAX];
+  strcpy(msg, "ERROR: ");
+  char buf[LOG_SIZE_MAX - strlen(msg)];
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf_P(buf, LOG_SIZE_MAX, fmt, ap);
+  va_end(ap);
+  strcat(msg, buf);
+  Serial.println(msg);
+}
---
+++ src/firmware/doorlock_controller/SerialLogger.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <Arduino.h>
+#include <stdarg.h>
+
+#define SERIAL_INFO(fmt, ...) SerialLogger::info(PSTR(fmt), ##__VA_ARGS__)
+#define SERIAL_ERROR(fmt, ...) SerialLogger::error(PSTR(fmt), ##__VA_ARGS__)
+
+class SerialLogger {
+  public:
+    Logger();
+    static void info(const char *fmt, ...);
+    static void error(const char *fmt, ...); 
+};
+
+
+   
