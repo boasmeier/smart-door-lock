a73a8b410384f80d6c91e267e4bdb679a7d03f29
boasmeier <62314872+boasmeier@users.noreply.github.com>
2021-11-03 13:42:36 +0100

    feat(firmware): use MqttLogger to send successfull connection to gateway

--- src/firmware/doorlock_controller/doorlock_controller.ino
+++ src/firmware/doorlock_controller/doorlock_controller.ino
@@ -28,20 +28,17 @@
 #include "MqttTopics.hpp"
 #include "secrets.hpp"
 #include "DoorlockConfig.h"
+#include "SerialLogger.hpp"
+#include "MqttLogger.hpp"
 
-#define LOG_PRINTFLN(fmt, ...)  printfln_P(PSTR(fmt), ##__VA_ARGS__)
-#define LOG_SIZE_MAX 128
-void printfln_P(const char *fmt, ...) {
-  char buf[LOG_SIZE_MAX];
-  va_list ap;
-  va_start(ap, fmt);
-  vsnprintf_P(buf, LOG_SIZE_MAX, fmt, ap);
-  va_end(ap);
-  Serial.println(buf);
-}
+const int buttonPin = 2;     // the number of the pushbutton pin
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+MyMqttClient *mqtt;
+WifiConnectionHandler *connHandl;
 
 // the setup function runs once when you press reset or power the board
-MyMqttClient *mqtt;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   Serial.begin(9600);
@@ -49,19 +46,49 @@ void setup() {
     continue;
   }
 
-  LOG_PRINTFLN("Test logger %d", 150);
-
-  char ssid[] = SECRET_SSID;
-  char pass[] = SECRET_PASS;
-  WifiConnectionHandler connHandl = WifiConnectionHandler();
-  connHandl.scanNetworks();
-  connHandl.connectWPA2(ssid, pass);
-  
+  // set up connection to gateway
+  connectToWifi();
   mqtt = new MyMqttClient(MQTT_HOST, MQTT_PORT);
+  logSuccessfullConnectionToCloud();
+  
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  mqtt->publish(MqttTopics::DOOR_STATE, "{ doorState: open }");
-  delay(5000);
+  if (buttonState == HIGH) {
+    // turn LED on:
+    mqtt->publish(MqttTopics::DOOR_STATE, "Ring event");
+  }
+  mqtt->publish(MqttTopics::DOOR_STATE, "{ \"doorState\": \"open\" }");
+  mqtt->publish(MqttTopics::LOCK_STATE, "{ \"lockState\": \"locked\" }");
+  
+  mqtt->publish(MqttTopics::INTRUSION_EVENT, "Intrusion event");
+  mqtt->publish(MqttTopics::SUSPICIOUS_ACTIVITY_EVENT, "Suspicious Activity");
+  mqtt->publish(MqttTopics::RING_EVENT, "Ring event");
+
+  MQTT_INFO(mqtt, "Test logger %d", 42);
+  
+  delay(10000);
+}
+
+void connectToWifi() {
+  char ssid[] = SECRET_SSID;
+  char pass[] = SECRET_PASS;
+  connHandl = new WifiConnectionHandler();
+  connHandl->scanNetworks();
+  connHandl->connectWPA2(ssid, pass);
+}
+
+void logSuccessfullConnectionToCloud() {
+  MQTT_INFO(mqtt, "Arduino connected to network with ssid: %s", connHandl->getSsid());
+  MQTT_INFO(mqtt, "IP Address of arduino: %s", connHandl->getIp().c_str());
+  byte mac[6];
+  connHandl->getMac(mac);
+  MQTT_INFO(mqtt, "MAC address of arduino: %02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  byte bssid[6];
+  connHandl->getBssid(bssid);
+  MQTT_INFO(mqtt, "BSSID: %02X:%02X:%02X:%02X:%02X:%02X", bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5]);
+  MQTT_INFO(mqtt, "Arduino connected to broker %s:%d", MQTT_HOST, MQTT_PORT);
 }
