cc94d059bc971916843ac512503e085a5b282d68
boasmeier <62314872+boasmeier@users.noreply.github.com>
2021-11-14 15:59:08 +0100

    feat(firmware): add interrupts to handle motion detection

--- src/firmware/doorlock_controller/src/door/MotionSensor.cpp
+++ src/firmware/doorlock_controller/src/door/MotionSensor.cpp
@@ -1,11 +1,21 @@
 #include "SPI.h"
 #include "MotionSensor.hpp"
+#include "../connection/MyMqttClient.hpp"
+#include "../connection/MqttTopics.hpp"
+#include "../logger/SerialLogger.hpp"
+#include "../logger/MqttLogger.hpp"
 
 MotionSensor::MotionSensor(int pin) : m_pin(pin) {
     pinMode(m_pin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(m_pin), handleMovementDetection, RISING);
 }
 
 MotionState MotionSensor::getState() {
     int state = digitalRead(m_pin);
     return state ? MotionState::ACTIVITY : MotionState::NO_ACTIVITY;
+}
+
+void handleMovementDetection() {
+    // TODO: Check wheter it is suspicious movement
+    SERIAL_INFO("Movement detected");
 }
