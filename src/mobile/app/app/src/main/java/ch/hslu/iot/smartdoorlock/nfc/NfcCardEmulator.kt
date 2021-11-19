11505edebb5d790c9e4460bbfdde85335e1991ad
tobiasheller1 <toebi12@gmail.com>
2021-11-19 16:10:33 +0100

    app: add nfc configuration (#31)

--- src/mobile/app/app/src/main/java/ch/hslu/iot/smartdoorlock/nfc/NfcCardEmulator.kt
+++ src/mobile/app/app/src/main/java/ch/hslu/iot/smartdoorlock/nfc/NfcCardEmulator.kt
@@ -1,4 +1,17 @@
 package ch.hslu.iot.smartdoorlock.nfc
 
-class NfcCardEmulator {
+import android.nfc.cardemulation.HostApduService
+import android.os.Bundle
+import android.util.Log
+
+class NfcCardEmulator : HostApduService() {
+    var TAG = "NfcCardEmulator"
+    override fun processCommandApdu(commandApdu: ByteArray, extras: Bundle?): ByteArray {
+        Log.i(TAG, "received data $commandApdu")
+        return ByteArray(0)
+    }
+
+    override fun onDeactivated(reason: Int) {
+        Log.i(TAG, "onDeactivated reason: $reason")
+    }
 }
