90c6edaed4ef3b7c84ebf9bdfd18d9ae4cbfef35
Niklas Tanner <niklas.tanner@hotmail.com>
2021-11-23 22:51:48 +0100

    Add simple navigation

--- src/visualisation/src/app/app.module.ts
+++ src/visualisation/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
+import {AppRoutingModule, routerComponents} from './app-routing.module';
 
 // Components
-import {DoorlistComponent} from './doorlist/doorlist.component';
 import {NavigationComponent} from './navigation/navigation.component';
-import {LoggerComponent} from './logger/logger.component';
 
 // REST
 import {HttpClientModule} from "@angular/common/http";
@@ -14,24 +12,25 @@ import {HttpClientModule} from "@angular/common/http";
 // Toast
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ToastrModule} from "ngx-toastr";
+
+// Materials
 import {MatButtonModule} from "@angular/material/button";
 import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DoorlistComponent,
     NavigationComponent,
-    LoggerComponent
+    routerComponents
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     ToastrModule.forRoot(),
     BrowserAnimationsModule,
     MatButtonModule,
-    MatTableModule,
-    AppRoutingModule
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
