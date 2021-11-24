a001a72befb2e6932737d16bbb0e535ede137ae3
Niklas Tanner <niklas.tanner@hotmail.com>
2021-11-24 12:13:28 +0100

    Remove unused Navigation

--- src/visualisation/src/app/app.module.ts
+++ src/visualisation/src/app/app.module.ts
@@ -1,10 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
-import {AppRoutingModule, routerComponents} from './app-routing.module';
-
-// Components
-import {NavigationComponent} from './navigation/navigation.component';
 
 // REST
 import {HttpClientModule} from "@angular/common/http";
@@ -16,16 +12,15 @@ import {ToastrModule} from "ngx-toastr";
 // Materials
 import {MatButtonModule} from "@angular/material/button";
 import {MatTableModule} from "@angular/material/table";
+import {DoorlistComponent} from "./doorlist/doorlist.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
-    routerComponents
+    DoorlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     ToastrModule.forRoot(),
     BrowserAnimationsModule,
