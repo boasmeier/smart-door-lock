2351e20dbabc05ffa58af712ccf39ef33e41488a
tobiasheller1 <toebi12@gmail.com>
2021-11-30 09:12:34 +0100

    gateway: new, play alarm if there is a break in

--- src/gateway/door_lock_edge_agent/src/models/bell.py
+++ src/gateway/door_lock_edge_agent/src/models/bell.py
@@ -1,5 +1,5 @@
 import logging
-import RPi.GPIO as GPIO 
+#import RPi.GPIO as GPIO 
 import threading
 import time
 from models.events import DoorLockEvent, DoorLockEventType
@@ -15,6 +15,11 @@ class Bell():
         """
         pass
 
+    def alarm(self, time_in_s: int):
+        """
+        Plays an alarm for a certain time.
+        """
+        pass
 class DummyBell(Bell):
     """
     A bell which logs instead of actually ringing.
@@ -25,6 +30,12 @@ class DummyBell(Bell):
         Writes a log if someone is ringing.
         """
         logging.info(f"Ring, Ring for {time_in_s} seconds")
+
+    def alarm(self, time_in_s: int):
+        """
+        Writes a log if there is an alarm.
+        """
+        logging.info(f"Alarm, Alarm for {time_in_s} seconds")
 
 class PiezoBell(Bell):
     """
@@ -41,6 +52,14 @@ class PiezoBell(Bell):
         logging.info(f"Piezo Bell: Ring, Ring for {time_in_s} seconds")
         ring = threading.Thread(target=self._ring, args=(time_in_s,))
         ring.start()
+
+    def alarm(self, time_in_s: int):
+        """
+        Plays the alarm for the piezo bell.
+        """
+        logging.info(f"Piezo Bell: Alarm, Alarm for {time_in_s} seconds")
+        alarm = threading.Thread(target=self._alarm, args=(time_in_s,))
+        alarm.start()
 
     def _ring(self, time_in_s: int):
         f_ding = 600
@@ -60,7 +79,21 @@ class PiezoBell(Bell):
         pwm2.start(50)                      
         time.sleep(time_in_s//2)
         pwm2.stop() 
-        GPIO.cleanup()                                     
+        GPIO.cleanup()   
+
+
+    def _alarm(self, time_in_s: int):
+        f_alarm = 600
+
+        for i in range(time_in_s//2):
+            GPIO.setmode(GPIO.BOARD)        
+            GPIO.setup(self.pin, GPIO.OUT)
+            pwm = GPIO.PWM(self.pin, f_alarm)   
+            pwm.start(50)                      
+            time.sleep(1)
+            pwm.stop() 
+            time.sleep(1)
+            GPIO.cleanup()     
 
 class BellHandler():
     def __init__(self, bell: Bell):
@@ -69,3 +102,6 @@ class BellHandler():
     def handle_event(self, event: DoorLockEvent):
         if event.event_type == DoorLockEventType.ring:
             self.bell.ring(5)
+
+        elif event.event_type == DoorLockEventType.intrusion:
+            self.bell.alarm(20)
